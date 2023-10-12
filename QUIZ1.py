d1eb9a62af4143b5fe5acf4474bf55c31b208d4f
Joshua Suco <atwings56@gmail.com>
2023-10-13 02:49:58 +0800

    First Commit

---
+++ QUIZ1.py
@@ -0,0 +1,6 @@
+
+litters = int(input("The price of a gasoline is P70, how much gasoline?"))
+price_gasoline = 70
+litters = litters * price_gasoline 
+
+print("it will be P" + str(litters))
---
+++ kilometers.py
@@ -0,0 +1,9 @@
+
+# Step 1: Assign the initial value of miles
+miles = 100
+
+# Step 2: Convert miles to kilometers using the value
+kilometers = miles * 1.609
+
+# Step 3: Print the calculated value of kilometers
+print(kilometers) # This will output the value of miles
